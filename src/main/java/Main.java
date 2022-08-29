8bc0aef537a021faf3cc8a7e6864dc8e862a9c2a
Mikhail Frolov <mf-1992@yandex.ru>
2022-08-29 13:53:58 +0300

    HW Adapter

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Ints intsCalc = new IntsCalculator();
+
+        System.out.println(intsCalc.sum(2, 2));
+        System.out.println(intsCalc.sub(10, 5));
+        System.out.println(intsCalc.mult(3, 9));
+        System.out.println(intsCalc.div(36, 3));
+        System.out.println(intsCalc.pow(2, 10));
+    }
+}
